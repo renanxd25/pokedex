23982d9fab168d4ab8f28359828f39f1f6cf56da
Renan Mendes <renanxd7x@hotmail.com>
2022-09-18 22:58:57 -0300

    adicionando service para ler a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 //modulos
 import { PagesModule } from './pages/pages.module';
@@ -15,6 +16,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule
     
   ],
   providers: [],
